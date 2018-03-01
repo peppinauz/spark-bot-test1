010ffecc171006b03325eccaca8f5a8c35a9c5c8
peppinauz <joseantonio.manzanor@gmail.com>
2018-03-01 16:07:09 +0100

    +++++ cambios

--- skills/command-help.js
+++ skills/command-help.js
@@ -8,6 +8,7 @@ module.exports = function (controller) {
         text += "\n- " + bot.enrichCommand(message, ".commons") + ": shows metadata about myself";
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
         text += "\n- " + bot.enrichCommand(message, "caracola") + ": spreads the word about my skills";
+        text += "\n- " + bot.enrichCommand(message, "add") + ": añade una persona al espacio";
         bot.reply(message, text);
     });
 }
